89b100386f743eefcd3bdd6ed413f4db7ed83d7a
DawgSlaya <tim.d.song@gmail.com>
2018-08-24 20:18:27 -0700

    Impliment the game

--- W2D1/Simon/lib/simon.rb
+++ W2D1/Simon/lib/simon.rb
@@ -1,12 +1,9 @@
+require 'byebug'
+
 class Simon
-  COLORS = %w(red blue green yellow)
 
-  attr_accessor :sequence_length, :game_over, :seq
-
-  def initialize
-    @sequence_length = 1
-    @game_over = false
-    @seq = []
+  def self.quick_play
+    Simon.new.play
   end
 
   def play
@@ -15,6 +12,18 @@ class Simon
     end
     game_over_message
     reset_game
+  end
+
+  private
+
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def take_turn
@@ -27,11 +36,45 @@ class Simon
   end
 
   def show_sequence
+    system("clear")
     add_random_color
+    @seq.each do |color|
+      puts color
+      sleep(1)
+      system("clear")
+    end
+    nil
   end
 
   def require_sequence
+    puts "Please enter sequence"
+    input_seq = []
+    i = 0
+    while i < @sequence_length
+      input_seq << parse_input_color
+      unless @seq[0...input_seq.length] == input_seq
+        @game_over = true
+        break
+      end
+      i += 1
+    end
+  end
 
+  def parse_input_color
+    color_hsh = {
+      r: "red",
+      b: "blue",
+      g: "green",
+      y: "yellow"
+    }
+    begin
+      input_color = color_hsh[gets[0].downcase.to_sym]
+      raise IOError if input_color.nil?
+    rescue
+      puts "That's not a valid color!"
+      retry
+    end
+    input_color
   end
 
   def add_random_color
@@ -39,16 +82,17 @@ class Simon
   end
 
   def round_success_message
-
+    puts "Round Success!"
   end
 
   def game_over_message
-
+    puts "Game Over. Final sequence length: #{@sequence_length}"
   end
 
   def reset_game
     @sequence_length = 1
     @game_over = false
     @seq = []
+    nil
   end
 end
