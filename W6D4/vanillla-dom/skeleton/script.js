7a1b9bf94f6ae7445d83956658a1d815eeab64ce
DawgSlaya <tim.d.song@gmail.com>
2018-09-27 07:13:11 -0700

    Finish favorite submit

--- W6D4/vanillla-dom/skeleton/script.js
+++ W6D4/vanillla-dom/skeleton/script.js
@@ -1,31 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // toggling restaurants
 
   const toggleLi = (e) => {
     const li = e.target;
-    if (li.className === "visited") {
-      li.className = "";
+    if (li.className === 'visited') {
+      li.className = '';
     } else {
-      li.className = "visited";
+      li.className = 'visited';
     }
   };
 
-  document.querySelectorAll("#restaurants li").forEach((li) => {
-    li.addEventListener("click", toggleLi);
+  document.querySelectorAll('#restaurants li').forEach((li) => {
+    li.addEventListener('click', toggleLi);
   });
-
-
 
   // adding SF places as list items
 
   // --- your code here!
+  const handleFavoriteSubmit = e => {
+    e.preventDefault();
 
+    const favoriteInput = document.querySelect('.favorite-input');
+    const favorite = favoriteInput.value;
+    favoriteInput.value = '';
 
+    const newListLi = document.createElement('li');
+    newListLi.textContent = favorite;
+
+    const favoriteList = document.getElementById('sf-places');
+    favoritesList.appendChild(newListLi);
+  };
+
+  constListSubmitButton = document.querySelector('.favorite-submit');
+  listSubmitButton.addEventListener('click', handleFavoriteSubmit);
 
   // adding new photos
 
   // --- your code here!
-
-
 
 });
