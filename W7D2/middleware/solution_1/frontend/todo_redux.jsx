b05bbf2ac0eff849026429e51f56c76824d1c2c1
DawgSlaya <tim.d.song@gmail.com>
2018-10-02 07:46:00 -0700

    Finish W7D2

--- W7D2/middleware/solution_1/frontend/todo_redux.jsx
+++ W7D2/middleware/solution_1/frontend/todo_redux.jsx
@@ -7,8 +7,37 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
+  let store = configureStore(preloadedState);
+
+  // store.dispatch = addLoggingToDispatch(store);
+
+  // store = applyMiddlewares(store, addLoggingToDispatch)
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+// const addLoggingToDispatch = (store) => {
+//   let disp = store.dispatch;
+//   return (action) => {
+//     console.log("old state: ", store.getState());
+//     console.log("action: ", action)
+//     disp(action);
+//     console.log("new state: ", store.getState());
+//   }
+// };
+
+// const addLoggingToDispatch = store => next => action => {
+//   console.log("old state: ", store.getState());
+//   console.log("action: ", action)
+//   next(action);
+//   console.log("new state: ", store.getState());
+// };
+//
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch;
+//   middlewares.forEach((middleware) => {
+//     dispatch = middleware(store)(dispatch);
+//   })
+//   return Object.assign({}, store, {dispatch})
+// };
